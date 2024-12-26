c8ef50792a44130867b2b3ec005cb86743c1d1a5
jose podermail <jose.podermail@gmail.com>
2024-12-26 12:41:26 -0600

    ajustes

--- rollapp/src/App.jsx
+++ rollapp/src/App.jsx
@@ -20,61 +20,89 @@ export const Turnos = () => {
       );
     });
   };
-const dayNow = ()=>{
-  let date =  Date.now()
-  
-  return new Date(date).toISOString().split("T")[0]
-}
+  const dayNow = () => {
+    let date = Date.now();
+
+    return new Date(date).toISOString().split("T")[0];
+  };
   return (
-    <div className="p-6 bg-white rounded-md shadow-md h-screen w-full mx-auto">
-    {/* Título */}
- <div className="h-[10%] ">
- <h1 className="text-2xl font-bold text-gray-700 mb-4 text-center">Turnos Programados del 25 de diciembre al 25 de febrero <br /> para lavar el baño</h1>
- 
- </div>
-  
-    {/* Lista de turnos */}
-    <ul className="space-y-4 h-[60%] overflow-y-auto w-full">
-      {shifts.map((shift) => (
-        <li
-          key={shift.id}
-          className="p-4 bg-gray-50 border border-gray-200 rounded-md shadow-sm flex justify-between items-center"
-        >
-          {/* Información del turno */}
-          <div className="text-gray-800">
-            <p>
-              <span className="font-semibold">Fecha:</span> {shift.date}
-            </p>
-            <p>
-              <span className="font-semibold">Usuario:</span> {shift.user_name}
-            </p>
-            <p>
-              <span className={`font-semibold ${shift.status === "pendiente" ? "text-yellow-600" : "text-green-600"}`}>
-                Estado:
-              </span>{" "}
-              {shift.status}
-            </p>
-          </div>
-  
-          {/* Botón "Completar" */}
-          {shift.status === "pendiente" && (
-            <button
-            disabled={shift.date != dayNow()}
-              onClick={() => markAsCompleted(shift.id)}
-              className={`ml-4 ${shift.date == new Date().toISOString().split("T")[0] ? "bg-green-500" : "bg-gray-500"} text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 transition`}
-            >
-              {
-                shift.date == new Date().toISOString().split("T")[0] ? "Termine" : "No disponible"
-              }
-             </button>
-          )}
-        </li>
-      ))}
-    </ul>
-    <Config/>
-  </div>
-  
+    <div className="p-6 flex flex-col  bg-white rounded-md shadow-md h-screen w-full mx-auto">
+      {/* Título */}
+      <div className="h-auto ">
+        <h1 className="text-2xl font-bold text-gray-700 mb-4 text-center">
+        Rol de limpieza (baño)
+        </h1>
+      </div>
+
+      {/* Lista de turnos */}
+      <ul className="space-y-4  overflow-y-auto w-full">
+        {shifts.map((shift) => (
+          <li
+            key={shift.id}
+            className={`p-4 ${ shift.date === dayNow()
+              ? "bg-green-100" 
+              : shift.date < dayNow() && shift.status === "pendiente"
+              ? "bg-orange-100" 
+              : "bg-gray-100" } border border-gray-200 rounded-md shadow-sm flex justify-between items-center`}
+          >
+            {/* Información del turno */}
+            <div className="text-gray-800">
+              <p>
+                <span className="font-semibold">Fecha:</span> {shift.date}
+              </p>
+              <p>
+                <span className="font-semibold">Usuario:</span>{" "}
+                {shift.user_name}
+              </p>
+              <p>
+                <span
+                  className={`font-semibold ${
+                    shift.status === "pendiente"
+                      ? "text-yellow-600"
+                      : "text-green-600"
+                  }`}
+                >
+                  Estado:
+                </span>{" "}
+                {shift.status}
+              </p>
+            </div>
+
+            {/* Botón "Completar" */}
+            {shift.status === "pendiente" && (
+              <button
+                disabled={ shift.date === dayNow()
+                  ? false 
+                  : shift.date < dayNow() && shift.status === "pendiente"
+                  ? true
+                  : true}
+                onClick={() => markAsCompleted(shift.id)}
+                className={`ml-4 ${
+                  shift.date === dayNow()
+                    ? "bg-green-500" 
+                    : shift.date < dayNow() && shift.status === "pendiente"
+                    ? "bg-orange-500" 
+                    : "bg-gray-500" 
+                } text-white font-semibold py-2 px-4 rounded  transition`}
+              >
+                {
+                  shift.date === dayNow()
+                    ? "Completar" 
+                    : shift.date < dayNow() && shift.status === "pendiente"
+                    ? "No marcado" 
+                    : "No disponible" 
+                }
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+      <div>
+        <p className="py-5 text-center text-blue-500 font-bold text-xl" >
+          Created by josé
+        </p>
+      </div>
+      {/* <Config /> */}
+    </div>
   );
 };
-
-
